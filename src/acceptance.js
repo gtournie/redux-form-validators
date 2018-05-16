92bb5470e8c97c4b1e8eeb89572d2ef279748192
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-05-16 10:51:28 +0200

    fixed acceptance

--- src/acceptance.js
+++ src/acceptance.js
@@ -1,13 +1,12 @@
-import Validators from './index'
-import { prepareMsg, prepare, memoize } from './helpers'
+import { formatMsg, prepareMsg, prepare, memoize, DEFAULT_OPTIONS } from './helpers'
 
 
 let acceptance = memoize(function ({ accept, message, msg, 'if': ifCond, unless }={}) {
   msg = msg || message
 
   return prepare(ifCond, unless, false, function (value) {
-    if ([].concat(accept || Validators.defaultOptions.accept).map(String).indexOf(value) < 0) {
-      return Validators.formatMessage(prepareMsg(msg, 'acceptance'))
+    if ([].concat(accept || DEFAULT_OPTIONS.accept).map(String).indexOf(value) < 0) {
+      return formatMsg(prepareMsg(msg, 'acceptance'))
     }
   })
 })
