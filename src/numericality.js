61d199c01e9ece8e1a3caf1360126a6edf7dccb9
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-05-16 11:00:15 +0200

    fixed numericality

--- src/numericality.js
+++ src/numericality.js
@@ -1,5 +1,4 @@
-import Validators from './index'
-import { prepareMsg, prepare, isNumber, selectNum, trunc, memoize } from './helpers'
+import { formatMsg, prepareMsg, prepare, isNumber, selectNum, trunc, memoize } from './helpers'
 
 
 let numericality = memoize(function ({
@@ -29,34 +28,34 @@ let numericality = memoize(function ({
 
   return prepare(ifCond, unless, allowBlank, function (value) {
     if (!isNumber(value)) {
-      return Validators.formatMessage(prepareMsg(msg, 'notANumber'))
+      return formatMsg(prepareMsg(msg, 'notANumber'))
     }
     if (int && (+value % 1)) {
-      return Validators.formatMessage(prepareMsg(msg, 'notAnInteger'))
+      return formatMsg(prepareMsg(msg, 'notAnInteger'))
     }
     if (null !== equal && +value !== equal) {
-      return Validators.formatMessage(prepareMsg(msg, 'equalTo', { count: equal }))
+      return formatMsg(prepareMsg(msg, 'equalTo', { count: equal }))
     }
     if (null !== diff && +value === diff) {
-      return Validators.formatMessage(prepareMsg(msg, 'otherThan', { count: diff }))
+      return formatMsg(prepareMsg(msg, 'otherThan', { count: diff }))
     }
     if (null !== greater && +value <= greater) {
-      return Validators.formatMessage(prepareMsg(msg, 'greaterThan', { count: greater }))
+      return formatMsg(prepareMsg(msg, 'greaterThan', { count: greater }))
     }
     if (null !== greaterOrEqual && +value < greaterOrEqual) {
-      return Validators.formatMessage(prepareMsg(msg, 'greaterThanOrEqualTo', { count: greaterOrEqual }))
+      return formatMsg(prepareMsg(msg, 'greaterThanOrEqualTo', { count: greaterOrEqual }))
     }
     if (null !== less && +value >= less) {
-      return Validators.formatMessage(prepareMsg(msg, 'lessThan', { count: less }))
+      return formatMsg(prepareMsg(msg, 'lessThan', { count: less }))
     }
     if (null !== lessOrEqual && +value > lessOrEqual) {
-      return Validators.formatMessage(prepareMsg(msg, 'lessThanOrEqualTo', { count: lessOrEqual }))
+      return formatMsg(prepareMsg(msg, 'lessThanOrEqualTo', { count: lessOrEqual }))
     }
     if (even && (trunc(+value) % 2)) {
-      return Validators.formatMessage(prepareMsg(msg, 'even'))
+      return formatMsg(prepareMsg(msg, 'even'))
     }
     if (odd && !(trunc(+value) % 2)) {
-      return Validators.formatMessage(prepareMsg(msg, 'odd'))
+      return formatMsg(prepareMsg(msg, 'odd'))
     }
   })
 })
